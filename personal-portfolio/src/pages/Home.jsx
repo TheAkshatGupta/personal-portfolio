e6c23b632beb405f8c3fa6be1746698dfbbeb1fd
TheAkshatGupta <2k23.csds2310601@gmail.com>
2026-03-02 14:14:45 +0530

    TailwindCSS Setup and Theme Customization

---
+++ personal-portfolio/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+export const Home = () =>{
+    return (
+    <div className = "min-h-screen container">
+
+        {/* Theme Toggle */}
+        {/* Background Effects */}
+        {/** Navbar */}
+        {/* Main Content */}
+        {/* Footer */}
+    </div>
+    );
+};
